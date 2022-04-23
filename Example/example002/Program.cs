0e15e4cd709b3ead1849625a63aa8d90bc239142
Пётр <dimok392@mail.ru>
2022-04-23 05:47:37 +0300

    Example002

---
+++ Example/example002/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(10,99); //Случайный отрезок от 1,,,9
+Console.WriteLine(numberA);
+int numberB = numberA/10;
+int numberC = numberA%10;
+int max;
+if (numberB>numberC) max = numberB;
+else max = numberC;
+Console.WriteLine(max);
