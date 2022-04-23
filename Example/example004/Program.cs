27b1980568f2960821104c74429ad7a70a664cb4
Пётр <dimok392@mail.ru>
2022-04-23 06:37:35 +0300

    Example004

---
+++ Example/example004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 23;
+int c =7;
+if (a%b==0&a%c==0)
+{
+    Console.WriteLine($"Число {a} одновременно кратно числу {b} и {c}");
+
+}
+else 
+{
+    Console.WriteLine($"Число {a} одновременно не кратно числу {b} и {c}");
+}
