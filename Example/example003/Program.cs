66467046eaff68f966ffa432984648d336c866e1
Пётр <dimok392@mail.ru>
2022-04-23 06:20:16 +0300

    Example003

---
+++ Example/example003/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите 1-е чило");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2-е чило");
+int b = int.Parse(Console.ReadLine());
+if (a%b==0)
+{
+
+ Console.WriteLine($"число {b} кратно числу {a}");
+
+}
+else 
+{
+    Console.WriteLine($"остаток {a%b} ");
+}
+
